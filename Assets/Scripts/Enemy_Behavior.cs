20e158b7ab865294cef89e0a0730e09400f674ab
Léonard Alarie <lelecopter@gmail.com>
2023-04-07 16:47:12 -0400

    Added almost functionning AI

--- Assets/Scripts/Enemy_Behavior.cs
+++ Assets/Scripts/Enemy_Behavior.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,10 +11,18 @@ public class Enemy_Behavior : MonoBehaviour
     public NavMeshAgent agent;
     public Transform player;
 
+    public Transform[] gommes;
+    private Transform ActiveGomme;
+
     public LayerMask whatIsGround, whatIsPlayer;
 
     public Vector3 WalkPoint;
     public bool WalkPointSet;
+
+    public int chasetimerMax = 60;
+    private int chasetimerCur;
+    private bool isChasingPlayer = false;
+    private bool canChasePlayer = true;
 
     public float WalkPointRange;
     public float SightRange;
@@ -21,38 +30,65 @@ public class Enemy_Behavior : MonoBehaviour
     
     void Start()
     {
-
-        
-    
-    }
-    
-    void Awake()
-    {
+        ActiveGomme = gommes[0];
+        //initialise la gomme la plus proche
+        SelectNewGomme();
+        chasetimerCur = chasetimerMax;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        PlayerinSightRange = Physics.CheckSphere(transform.position, SightRange, whatIsPlayer);
-
-        if (PlayerinSightRange)
+        //si en poursuite
+        if(isChasingPlayer)
             ChasePlayer();
+
+        //sinon, vérifier si le joueur est plus près que la gomme la plus proche
+        if (Vector3.Distance(ActiveGomme.position, transform.position) >
+            Vector3.Distance(player.position, transform.position))
+        {
+            //timer pour que cette vérification ne soit fait qu'une fois par seconde
+            chasetimerCur = chasetimerMax;
+            isChasingPlayer = true;
+            ChasePlayer();
+        }
         
-        else Patrol();
+        else ChaseGum();
     }
 
-    void Patrol()
+    void ChaseGum()
     {
-        if (!WalkPointSet)
-            SetWalkPoint();
+        agent.SetDestination(ActiveGomme.position);
     }
-
-    void SetWalkPoint()
-    {
-        
-    }
+    
     private void ChasePlayer()
     {
+        //poursuit le joueur. Si le timer se termine, on arrête de le suivre par défaut et on peut vérifier dans update
+        //qui est le plus proche
         agent.SetDestination(player.position);
+        chasetimerCur--;
+        if (chasetimerCur <= 0)
+        {
+            isChasingPlayer = false;
+            SelectNewGomme();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //quand tu collisionne avec une gomme, en choisit une nouvelle
+        SelectNewGomme();
+    }
+
+    private void SelectNewGomme()
+    {
+        foreach (Transform gomme in gommes)
+        {
+            float distance = Vector3.Distance(gomme.position, transform.position);
+            if (distance < Vector3.Distance(ActiveGomme.position, transform.position))
+            {
+                ActiveGomme = gomme;
+            }
+        }
     }
 }
